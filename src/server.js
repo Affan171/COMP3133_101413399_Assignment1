0315d30221e779121e95829e67e15ab453b6ff4d
Affan <Affanshaikh76112@gmail.com>
2025-02-16 13:19:29 -0500

    Enabled GraphQL Playground in production

--- src/server.js
+++ src/server.js
@@ -1,43 +1,3 @@
-// const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/database');
-// const typeDefs = require('./graphql/typeDefs');
-// const resolvers = require('./graphql/resolvers');
-
-// dotenv.config();
-
-// // Connect to MongoDB
-// connectDB();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// // Initialize Apollo Server
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     context: ({ req }) => {
-//         const user = authMiddleware(req); // Extract userId from token
-//         return { req, user };
-//     },
-// });
-
-// async function startServer() {
-//     await server.start();
-//     server.applyMiddleware({ app });
-
-//     const PORT = process.env.PORT || 4000;
-//     app.listen(PORT, () => {
-//         console.log(`🚀 Server running on http://localhost:${PORT}${server.graphqlPath}`);
-//     });
-// }
-
-// startServer();
-
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -45,9 +5,10 @@ const { ApolloServer } = require("apollo-server-express");
 const jwt = require("jsonwebtoken");
 
 const connectDB = require("./config/database");
-const schema = require("./graphql/schema");  // Import our combined schema
+const schema = require("./graphql/schema");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -55,7 +16,7 @@ connectDB();
 
 async function startApolloServer() {
   const apolloServer = new ApolloServer({
-    schema,  // now we just pass in the merged schema
+    schema,
     context: ({ req }) => {
       const authHeader = req.headers.authorization || "";
       let user = null;
@@ -69,6 +30,8 @@ async function startApolloServer() {
       }
       return { user };
     },
+    introspection: true,  
+    playground: true,
   });
 
   await apolloServer.start();
@@ -76,8 +39,7 @@ async function startApolloServer() {
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+    console.log(`🚀 Server running at: https://comp3133-101413399-assignment1-1.onrender.com/graphql`);
   });
 }
 
